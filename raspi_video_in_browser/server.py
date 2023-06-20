76258448bb9dc9a52cb25b32aa845e842237b886
YELETIMAHALAKSHMI <94279266+YELETIMAHALAKSHMI@users.noreply.github.com>
2023-06-20 17:46:56 +0530

    Update server.py

--- raspi_video_in_browser/server.py
+++ raspi_video_in_browser/server.py
@@ -2,60 +2,45 @@ import cv2
 import numpy as np
 from flask import Flask, render_template, Response, request
 import threading
+from ultralytics import YOLO
 
 app = Flask(__name__)
-frame = None
-frame_size = None
+current_frame = None
+model = YOLO("yolov5s.pt")
 
 @app.route('/')
 def home():
     return "Welcome to Video Streaming Server!!!"
 
-def generate_frames():
-    global frame, frame_size
-    while True:
-        if frame is not None and frame_size is not None:
-            img = np.frombuffer(frame, np.uint8)
-            img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-            img = cv2.resize(img, frame_size)
-
-            ret, buffer = cv2.imencode('.jpg', img)
-            frame = buffer.tobytes()
-
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
-
-    cv2.destroyAllWindows()
-
-@app.route('/video_feed', methods=['GET', 'POST'])
+@app.route('/video_feed', methods=['POST'])
 def video_feed():
-    global frame, frame_size
-    if request.method == 'POST':
-        frame_data = request.data
-        frame_width = int(request.headers.get('Frame-Width'))
-        frame_height = int(request.headers.get('Frame-Height'))
-        frame_size = (frame_width, frame_height)
-        frame = np.frombuffer(frame_data, np.uint8)
-        print('Frame received from client:', frame.shape)
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    global current_frame
 
+    frame_data = request.data
+    frame = np.frombuffer(frame_data, np.uint8)
+    img = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    # Process the frame here using the model or any other operations
+    # Example: processed_frame = model.detect(img)
+
+    ret, buffer = cv2.imencode('.jpg', img)
+    current_frame = buffer.tobytes()
+
+    return "Video frame received."
+
+@app.route('/get_frame')
+def get_frame():
+    return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def generate():
+    global current_frame
+
+    while True:
+        if current_frame is not None:
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + current_frame + b'\r\n')
 
 def process_frame():
-    global frame, frame_size
     while True:
-        if frame is None or frame_size is None:
-            continue
-
-        img = np.frombuffer(frame, np.uint8)
-        img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-        img = cv2.resize(img, frame_size)
-
-        cv2.imshow("Object Detection", img)
-
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             break

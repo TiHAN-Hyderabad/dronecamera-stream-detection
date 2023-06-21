275eb4ac945059184a913ad04b01e2f073fb59eb
YELETIMAHALAKSHMI <94279266+YELETIMAHALAKSHMI@users.noreply.github.com>
2023-06-21 12:09:46 +0530

    Update server.py

--- raspi_video_in_browser/server.py
+++ raspi_video_in_browser/server.py
@@ -1,16 +1,16 @@
 import cv2
 import numpy as np
-from flask import Flask, render_template, Response, request
+from flask import Flask, Response, request
 import threading
 from ultralytics import YOLO
 
 app = Flask(__name__)
 current_frame = None
-model = YOLO("yolov5s.pt")
+model = YOLO("yolov5s.pt")  # Create the YOLO model
 
 # Specify the desired dimensions for the resized frames
-RESIZED_WIDTH = 640
-RESIZED_HEIGHT = 480
+RESIZED_WIDTH = 1200
+RESIZED_HEIGHT = 800
 
 @app.route('/')
 def home():
@@ -23,12 +23,25 @@ def video_feed():
     frame_data = request.data
     frame = np.frombuffer(frame_data, np.uint8)
     img = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-    
     frame_width = int(request.headers['Frame-Width'])
     frame_height = int(request.headers['Frame-Height'])
 
     # Resize the frame to desired dimensions
     img = cv2.resize(img, (RESIZED_WIDTH, RESIZED_HEIGHT))
+
+    # Perform object detection on the frame
+    results = model.predict(img)
+
+    for result in results:
+        for obj in result.boxes:
+            x1, y1, x2, y2 = map(int, obj.xyxy[0])  # Convert coordinates to integers
+            label = result.names[int(obj.cls[0])]  # Get the class label
+            confidence = obj.conf[0]  # Get the confidence score
+
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(img, f'{label}: {confidence:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+
 
 
     ret, buffer = cv2.imencode('.jpg', img)
@@ -58,4 +71,4 @@ def process_frame():
 
 if __name__ == '__main__':
     threading.Thread(target=process_frame).start()
-    app.run(host='0.0.0.0', port=9000)
+    app.run(host='0.0.0.0', port=9001)

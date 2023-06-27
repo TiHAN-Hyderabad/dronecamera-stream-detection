cece08ef1a47dbade2f2091e6a7e3ca9891f3ecf
YELETIMAHALAKSHMI <94279266+YELETIMAHALAKSHMI@users.noreply.github.com>
2023-06-27 22:15:54 +0530

    Update display.py

--- raspi_video_in_browser/display.py
+++ raspi_video_in_browser/display.py
@@ -1,26 +1,30 @@
 import cv2
-from flask import Flask, Response
+from flask import Flask, Response, request
 import threading
 
 app = Flask(__name__)
-frame_file = 'processed_frame.jpg'  # File storing the processed frame
+current_frame = None
 
 @app.route('/')
 def home():
     return "Welcome to Video Display Server!!!"
 
-@app.route('/display')
-def display():
-    return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/get_frame', methods=['POST'])
+def get_frame():
+    global current_frame
+    current_frame = request.data
+    return "Frame received."
 
-def generate():
+def generate_frames():
+    global current_frame
     while True:
-        frame = cv2.imread(frame_file)  # Read the processed frame from the file
-        if frame is not None:
-            _, buffer = cv2.imencode('.jpg', frame)
-            current_frame = buffer.tobytes()
+        if current_frame is not None:
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + current_frame + b'\r\n')
+
+@app.route('/display')
+def display():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def run_display_server():
     app.run(host='0.0.0.0', port=8081)

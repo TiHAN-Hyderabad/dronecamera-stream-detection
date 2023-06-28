5621bc8877b9a1382c2ce96f1b2089306f472dc7
YELETIMAHALAKSHMI <94279266+YELETIMAHALAKSHMI@users.noreply.github.com>
2023-06-28 16:12:01 +0530

    Update server.py

--- raspi_video_in_browser/server.py
+++ raspi_video_in_browser/server.py
@@ -6,12 +6,16 @@ import threading
 from ultralytics import YOLO
 
 app = Flask(__name__)
-frame_url = 'http://192.168.252.225:8081/get_frame'  # URL of the display server's get_frame endpoint
+frame_url = 'http://192.168.21.159:8081/get_frame'  # URL of the display server's get_frame endpoint
 
 # Specify the desired dimensions for the resized frames
 RESIZED_WIDTH = 1200
 RESIZED_HEIGHT = 800
 model = YOLO("yolov5s.pt")  # Create the YOLO model
+
+frame_received_count = 0  # Track the number of frames received
+frame_processed_count = 0  # Track the number of frames processed
+frame_sent_count = 0  # Track the number of frames sent to the display
 
 def send_frame(frame):
     requests.post(frame_url, data=frame, headers={'Content-Type': 'image/jpeg'})
@@ -22,11 +26,16 @@ def home():
 
 @app.route('/video_feed', methods=['POST'])
 def video_feed():
+    global frame_received_count, frame_processed_count, frame_sent_count
+
     frame_data = request.data
     frame = np.frombuffer(frame_data, np.uint8)
     img = cv2.imdecode(frame, cv2.IMREAD_COLOR)
     frame_width = int(request.headers['Frame-Width'])
     frame_height = int(request.headers['Frame-Height'])
+
+    frame_received_count += 1
+    print('Frames received from client:', frame_received_count)
 
     # Resize the frame to desired dimensions
     img = cv2.resize(img, (RESIZED_WIDTH, RESIZED_HEIGHT))
@@ -43,10 +52,15 @@ def video_feed():
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.putText(img, f'{label}: {confidence:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
+    frame_processed_count += 1
+    print('Frames processed:', frame_processed_count)
 
     ret, buffer = cv2.imencode('.jpg', img)
     frame_data = buffer.tobytes()
     threading.Thread(target=send_frame, args=(frame_data,)).start()
+
+    frame_sent_count += 1
+    print('Frames sent to display:', frame_sent_count)
 
     return "Frame processed."
 

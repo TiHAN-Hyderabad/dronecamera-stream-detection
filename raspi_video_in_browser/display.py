2ee301bbb0fc382e0e55d8a4eccc6733e0d940d0
YELETIMAHALAKSHMI <94279266+YELETIMAHALAKSHMI@users.noreply.github.com>
2023-06-28 16:12:18 +0530

    Update display.py

--- raspi_video_in_browser/display.py
+++ raspi_video_in_browser/display.py
@@ -5,20 +5,31 @@ import threading
 app = Flask(__name__)
 current_frame = None
 
+frame_received_count = 0  # Track the number of frames received
+frame_sent_count = 0  # Track the number of frames sent to the browser URL
+
 @app.route('/')
 def home():
     return "Welcome to Video Display Server!!!"
 
 @app.route('/get_frame', methods=['POST'])
 def get_frame():
-    global current_frame
+    global current_frame, frame_received_count
+
+    frame_received_count += 1
+    print('Frames received from server:', frame_received_count)
+
     current_frame = request.data
     return "Frame received."
 
 def generate_frames():
-    global current_frame
+    global current_frame, frame_sent_count
+
     while True:
         if current_frame is not None:
+            frame_sent_count += 1
+            print('Frames sent to browser URL:', frame_sent_count)
+
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + current_frame + b'\r\n')
 

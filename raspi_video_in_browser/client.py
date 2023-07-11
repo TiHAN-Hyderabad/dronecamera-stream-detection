49cb14ea4333a4476a42e85d30fd276470074f3e
YELETIMAHALAKSHMI <94279266+YELETIMAHALAKSHMI@users.noreply.github.com>
2023-07-11 22:53:17 +0530

    Update client.py

--- raspi_video_in_browser/client.py
+++ raspi_video_in_browser/client.py
@@ -1,7 +1,8 @@
 import cv2
 import requests
 import struct
-import imutils
+import time
+import datetime
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 
@@ -10,7 +11,7 @@ camera.resolution = (640, 480)
 camera.framerate = 24
 raw_capture = PiRGBArray(camera, size=(640, 480))
 
-server_url = 'http://host_ip:port/video_feed' # Replace server host_ip and port
+server_url = 'http://192.168.103.220:8080/video_feed'
 
 frames_sent_to_server = 0
 
@@ -18,25 +19,30 @@ try:
     print('Connected to Cache Server.')
     for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
         image = frame.array
-        image = imutils.resize(image, width=320)
+        image = cv2.resize(image, (320, 240))  # Resize the frame
 
         # Convert the frame to JPEG format
         _, buffer = cv2.imencode('.jpg', image)
         frame_data = buffer.tobytes()
 
-        # Send the frame to the server
         headers = {
             'Content-Type': 'application/octet-stream',
             'Frame-Width': str(image.shape[1]),
-            'Frame-Height': str(image.shape[0])
+            'Frame-Height': str(image.shape[0]),
+            'Client-Timestamp': str(time.time())
         }
+        start_time = time.time()
         response = requests.post(server_url, data=frame_data, headers=headers)
+        end_time = time.time()
+        delay = end_time - start_time
         print('Frame sent to server:', response.status_code)
+        print('Delay time (client to server):', delay)
 
         frames_sent_to_server += 1
-
-        # Print frame count for each frame
         print("Frame count:", frames_sent_to_server)
+
+        current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+        print('Frame sent by client at:', current_time)
 
         cv2.imshow("TRANSMITTING TO CACHE SERVER", image)
 

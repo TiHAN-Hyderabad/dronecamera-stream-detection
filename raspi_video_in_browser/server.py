1142bc23217f85c5f8b11918da62a784ea4bab94
YELETIMAHALAKSHMI <94279266+YELETIMAHALAKSHMI@users.noreply.github.com>
2023-07-11 21:49:43 +0530

    Update server.py

--- raspi_video_in_browser/server.py
+++ raspi_video_in_browser/server.py
@@ -6,6 +6,8 @@ from ultralytics import YOLO
 
 app = Flask(__name__)
 current_frame = None
+frame_count_client = 0
+frame_count_server = 0
 model = YOLO("yolov5s.pt")  # Create the YOLO model
 
 # Specify the desired dimensions for the resized frames
@@ -16,46 +18,43 @@ RESIZED_HEIGHT = 800
 def home():
     return "Welcome to Video Streaming Server!!!"
 
-@app.route('/video_feed', methods=['POST'])
+@app.route('/video_feed', methods=['POST', 'GET'])
 def video_feed():
-    global current_frame
+    global current_frame, frame_count_client, frame_count_server
+    if request.method == 'POST':
+        frame_data = request.data
+        frame = np.frombuffer(frame_data, np.uint8)
+        img = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+        frame_width = int(request.headers['Frame-Width'])
+        frame_height = int(request.headers['Frame-Height'])
 
-    frame_data = request.data
-    frame = np.frombuffer(frame_data, np.uint8)
-    img = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-    frame_width = int(request.headers['Frame-Width'])
-    frame_height = int(request.headers['Frame-Height'])
+        # Resize the frame to desired dimensions
+        img = cv2.resize(img, (RESIZED_WIDTH, RESIZED_HEIGHT))
 
-    # Resize the frame to desired dimensions
-    img = cv2.resize(img, (RESIZED_WIDTH, RESIZED_HEIGHT))
+        # Perform object detection on the frame
+        results = model.predict(img)
+        for result in results:
+            for obj in result.boxes:
+                x1, y1, x2, y2 = map(int, obj.xyxy[0])  # Convert coordinates to integers
+                label = result.names[int(obj.cls[0])]  # Get the class label
+                confidence = obj.conf[0]  # Get the confidence score
 
-    # Perform object detection on the frame
-    results = model.predict(img)
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.putText(img, f'{label}: {confidence:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-    for result in results:
-        for obj in result.boxes:
-            x1, y1, x2, y2 = map(int, obj.xyxy[0])  # Convert coordinates to integers
-            label = result.names[int(obj.cls[0])]  # Get the class label
-            confidence = obj.conf[0]  # Get the confidence score
+        ret, buffer = cv2.imencode('.jpg', img)
+        current_frame = buffer.tobytes()
+        frame_count_client += 1
+        frame_count_server += 1
+        print("Received frame from client. Total frames received:", frame_count_client)
+        print("Total frames processed:", frame_count_server)
 
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(img, f'{label}: {confidence:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-
-
-
-    ret, buffer = cv2.imencode('.jpg', img)
-    current_frame = buffer.tobytes()
-
-    return "Video frame received."
-
-@app.route('/get_frame')
-def get_frame():
-    return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        return "Video frame received."
+    else:
+        return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def generate():
     global current_frame
-
     while True:
         if current_frame is not None:
             yield (b'--frame\r\n'
